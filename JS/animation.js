3000a54dbf403fa858f368e9f6a714bd7ca6557a
Khen <palerkhen40@gmail.com>
2023-04-23 20:19:08 +0800

    adding animation when login is clicked

---
+++ JS/animation.js
@@ -0,0 +1,18 @@
+// Select the login button element
+const loginButton = document.querySelector('.login-button');
+
+// Add a click event listener to the login button element
+loginButton.addEventListener('click', function(event) {
+  // Prevent the default behavior of the link
+  event.preventDefault();
+
+  // Use GSAP to fade out the landing page content
+  gsap.to('body', {
+    duration: 0.5,
+    opacity: 0,
+    onComplete: function() {
+      // Once the fade-out animation is complete, redirect to the login page
+      window.location.href = 'login.html';
+    }
+  });
+});
